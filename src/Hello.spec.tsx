468c2f952ec17e0ba4c73bc8f3c80c94e184846c
alexvictoor <alexvictoor@gmail.com>
2016-10-06 23:49:48 +0200

    Initial version

---
+++ src/Hello.spec.tsx
@@ -0,0 +1,17 @@
+import { expect } from 'chai';
+import * as React from 'react';
+import { shallow, mount } from 'enzyme';
+
+import Hello from './Hello';
+
+describe('Hello', () => {
+
+    it('should say Hello!', () => {
+ 
+      const wrapper = shallow(<Hello/>);
+
+      expect(wrapper.find("div")).to.have.length(1);
+
+    });
+
+});
